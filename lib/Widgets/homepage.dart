be89f37521c459becb0abbcfb519e7c8c508fcec
Xizhe Wang <Artalxing@gmail.com>
2025-04-08 20:24:29 -0700

    Added some new files and worked on map

---
+++ lib/Widgets/homepage.dart
@@ -0,0 +1,90 @@
+﻿import 'dart:ffi';
+import 'package:flutter_lakewood_app/globleVars.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_lakewood_app/main.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:flutter_lakewood_app/Classes/mappointdata.dart';
+
+class Homepage extends StatefulWidget{
+  const Homepage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Homepage> createState() => HomepageState();
+}
+
+class HomepageState extends State<Homepage>{
+
+  MapPointData? currentData;
+
+  void updateCenter(LatLng center){
+    controller.moveAndRotate(center, focusZoom, 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+            'House Dating Sim UwU' //TODO: Name Here! Change for production.
+        ),
+      ),
+      body: map(),
+    );
+  }
+
+  Widget map() {
+    List<Widget> children = [];
+    children.add(openStreetMapTileLayer);
+    children.add(hotSingleHouses);
+
+    if(currentData != null){
+      //Add the basic overview screen.
+
+    }
+
+    return FlutterMap(options: const MapOptions(
+      initialCameraFit: CameraFit.coordinates(coordinates: [ //used https://www.latlong.net/ DON'T USE GOOGLE MAPS!!!
+        LatLng(45.418644, -122.679640), //corner 1
+        LatLng(45.404050, -122.651485) //corner 2
+      ]),
+      interactionOptions:
+      InteractionOptions(flags: ~InteractiveFlag.pinchZoom),
+
+    ),
+      mapController: controller,
+      children: [
+        openStreetMapTileLayer,
+        hotSingleHouses
+      ],
+    );
+  }
+}
+
+TileLayer get openStreetMapTileLayer => TileLayer(
+    urlTemplate: mapApiUrl,
+    userAgentPackageName: userPackageName
+);
+
+MarkerLayer get hotSingleHouses => MarkerLayer(markers: [
+  //Use code to parse a database into markers.
+  Marker(
+      point: const LatLng(45.413338, -122.667718),
+      width: mapIconWidth,
+      height: mapIconHeight,
+      alignment: Alignment.centerLeft,
+      child:GestureDetector(
+          onTap: () {
+            //Create a webpage using args specified in the database.
+          },
+          child: Icon(
+              Icons.location_pin,
+              size: mapIconSize,
+              color: Colors.red)
+      )
+  )
+]);
+
+MapController get controller => MapController();
---
+++ lib/globleVars.dart
@@ -0,0 +1,20 @@
+﻿//<editor-fold desc="Map Vars">
+
+//<editor-fold desc="Map Details">
+double mapIconWidth = 60;
+double mapIconHeight = 60;
+double mapIconSize = 60;
+
+double focusZoom = 25;
+
+//focusmap
+double focusMapInitZoom = 20;
+//</editor-fold>
+//<editor-fold desc="Map API Information">
+String mapApiUrl = "https://tile.openstreetmap.org/{z}/{x}/{y}.png"; //Change based on url here if default url not good enough https://wiki.openstreetmap.org/wiki/Raster_tile_providers
+String userPackageName = "com.lakeridge.app"; //May need changing
+
+
+//</editor-fold>
+
+//</editor-fold>
