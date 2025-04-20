4eb860fcbfe6898b4b3b64e0557dad403d4fe0f1
snoebal <li00806274@loswego.k12.or.us>
2025-04-19 21:43:03 -0700

    added page switching capabilities

--- lib/main.dart
+++ lib/main.dart
@@ -2,92 +2,133 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(HomePage());
+  runApp(MyApp());
 }
 
-class HomePage extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Widget homePage;
+  late List<Widget> widgetOptions;
+
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    homePage = Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigoAccent,
+        title: const Text("Home Page"),
+      ),
+
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/randombg.jpeg'),
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+            Expanded(
+              child: SizedBox.expand(
+                child: Padding(
+                  padding: EdgeInsets.all(40),
+                  child: Text(
+                    "Welcome to Lakewood",
+                    textAlign: TextAlign.center,
+
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "robotoSlab",
+                      color: Colors.white,
+                      fontSize: 52,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.all(40),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                ),
+                onPressed: () {
+                  _pageSwitch(1);
+                },
+                child: Text(
+                  "Get started",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: "gothicExpanded",
+
+                    color: Colors.white,
+                    fontSize: 26,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    widgetOptions = <Widget>[
+      homePage,
+      Text("PAGE 1 (get started button takes u here)"),
+      Text("PAGE 2"),
+    ];
+  }
+
+  void _pageSwitch(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.indigoAccent,
-          title: const Text("Home Page"),
-        ),
-
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/randombg.jpeg'),
-              fit: BoxFit.fitHeight,
-            ),
-          ),
-
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-
-            children: [
-              Expanded(
-                child: SizedBox.expand(
-                  child: Padding(
-                    padding: EdgeInsets.all(40),
-                    child: Text(
-                      "Welcome to Lakewood",
-                      textAlign: TextAlign.center,
-
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontFamily: "robotoSlab",
-                        color: Colors.white,
-                        fontSize: 52,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-
-              Padding(
-                padding: EdgeInsets.all(40),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueAccent,
-                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                  ),
-                  onPressed: () {
-                    print('button pressed!');
-                  },
-                  child: Text(
-                    "Get started",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: "gothicExpanded",
-
-                      color: Colors.white,
-                      fontSize: 26,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+        body: Center(child: widgetOptions.elementAt(_currentIndex)),
 
         bottomNavigationBar: BottomNavigationBar(
+          onTap: _pageSwitch,
           items: const [
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
-              label: "Button 1",
+              label: "Home",
               tooltip: "Testing button",
             ),
 
             BottomNavigationBarItem(
-              icon: Icon(Icons.lightbulb_circle_outlined),
-              label: "Button 2",
+              icon: Icon(Icons.map),
+              label: "Map",
             ),
 
-            BottomNavigationBarItem(icon: Icon(Icons.map), label: "Button 3"),
+            BottomNavigationBarItem(icon: Icon(Icons.account_box), label: "Account"),
           ],
         ),
       ),
@@ -95,20 +136,33 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class TestPage extends StatelessWidget {
-  const TestPage({super.key});
+// class _TestPage extends StatelessWidget {
+//   int _selectedIndex = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(middle: Text("test text!")),
-      child: Center(
-        child: CupertinoButton(
-          onPressed: () {
+//   static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+//   static const List<Widget> _widgetOptions = <Widget>[
+//     Text('Index 0: Home', style: optionStyle),
+//     Text('Index 1: Business', style: optionStyle),
+//     Text('Index 2: School', style: optionStyle),
+//   ];
+
+//   void _pageSwitch(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//   });
+// }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return CupertinoPageScaffold(
+//       navigationBar: const CupertinoNavigationBar(middle: Text("test text!")),
+//       child: Center(
+//         child: CupertinoButton(
+//           onPressed: () {
             
-          }, 
-          child: Text("button!")),
-      ),
-    );
-  }
-}
+//           }, 
+//           child: Text("button!")),
+//       ),
+//     );
+//   }
+// }
