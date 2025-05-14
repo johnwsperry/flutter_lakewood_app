26226b347bb9ed7a0b6a040ab83dabf93fad9c06
Xizhe Wang <Artalxing@gmail.com>
2025-05-13 19:55:27 -0700

    Cleaned Up the Merge

--- lib/main.dart
+++ lib/main.dart
@@ -16,35 +16,3 @@ class MyApp extends StatelessWidget {
 }
 
 
-
-// class _TestPage extends StatelessWidget {
-//   int _selectedIndex = 0;
-// return MaterialApp(home: MapPage());
-
-//   static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-//   static const List<Widget> _widgetOptions = <Widget>[
-//     Text('Index 0: Home', style: optionStyle),
-//     Text('Index 1: Business', style: optionStyle),
-//     Text('Index 2: School', style: optionStyle),
-//   ];
-
-//   void _pageSwitch(int index) {
-//     setState(() {
-//       _selectedIndex = index;
-//   });
-// }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return CupertinoPageScaffold(
-//       navigationBar: const CupertinoNavigationBar(middle: Text("test text!")),
-//       child: Center(
-//         child: CupertinoButton(
-//           onPressed: () {
-            
-//           }, 
-//           child: Text("button!")),
-//       ),
-//     );
-//   }
-// }
