7b53ce097ecef0e5633c0b3c0d0710447d0263d6
Xizhe Wang <Artalxing@gmail.com>
2025-05-13 19:34:56 -0700

    Worked on it a bit more. Need to merge

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //return MaterialApp(home: HomePage());
+    return MaterialApp(home: HomePage());
     return MaterialApp(home: MapPage(title: 'hi',));
   }
 }
