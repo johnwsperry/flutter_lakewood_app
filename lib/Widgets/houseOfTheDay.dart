c3b76a4ad4fb4693a4d5d976f2e9e82a62a7ec90
Xizhe Wang <Artalxing@gmail.com>
2025-05-31 11:05:47 -0700

    Worked more on database

---
+++ lib/Widgets/houseOfTheDay.dart
@@ -0,0 +1,20 @@
+﻿import 'dart:math';
+
+import '../Database/databases.dart' as database;
+import '../Classes/mapData.dart';
+
+
+
+class HouseOfTheDay{
+
+  late int houseToday;
+
+  void displayHouses() async{
+    List<MapData> data = await database.houses();
+    if(houseToday == null){
+      houseToday = Random(DateTime.timestamp().day).nextInt(data.length);
+    }
+  }
+
+
+}
