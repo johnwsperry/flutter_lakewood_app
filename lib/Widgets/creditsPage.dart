81b6c933f1c69219afb22aac9644b3f3c4003f84
Xizhe Wang <Artalxing@gmail.com>
2025-07-05 10:46:53 -0700

    Updated creditsPage.dart

--- lib/Widgets/creditsPage.dart
+++ lib/Widgets/creditsPage.dart
@@ -106,7 +106,7 @@ class CreditsPage{
                                         fontFamily: "robotoSlab",
                                         fontSize: 12,
                                       ),
-                                      maxLines: 2,
+                                      maxLines: 5,
                                       overflow: TextOverflow.ellipsis,
                                     ),
                                   ],
@@ -134,15 +134,21 @@ class CreditsPage{
   //Data
   static List<String> names = [
     "Alvin Wang",
+    "Leon Li",
     "CJ Hanson",
+    "Allan Wang"
   ];
   static List<String> descriptions = [
-    "Placeholder",
-    "High school student, app contributor"
+    "High School Student.\nGuy who worked on most of the backend.\nAlso the IT guy.",
+    "I accidentally became the frontend developer lmao\nit's not a bug, it's a FEATURE!!",
+    "High school student, app contributor",
+    "placeholder"
   ];
   static List<String> imagePaths = [
     "AlvinWang-Credits-0.png",
-    "CJHanson-Credits-0.png"
+    "LeonLi-Credits-0.png",
+    "CJHanson-Credits-0.png",
+    "AllanWang-Credits-0.png"
   ];
 
 }
